f9dd37b6ce59a2b971791ca518a97a36ad9960ce
Gagoi <zangla.jeremy@gmail.com>
2019-02-20 22:40:05 +0100

    Ajout d'une particule à durée de vie limitée.

---
+++ fr/game_engine/graphics/elements/LifeLimitedParticle.java
@@ -0,0 +1,43 @@
+package fr.game_engine.graphics.elements;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class LifeLimitedParticle extends Particle {
+
+	public static final int IMMORTAL = -1;
+	
+	private final int lifeTime; // Durée de vie en nombre d'update (équivalent au nombre de déplacementà faire). (-1 pour immortelle).
+	private int life = 0; // Temps de vie (équivalen au nombre de déplacements fait).
+
+	public LifeLimitedParticle(final int x, final int y, final int lifeTime) {
+		super(x, y);
+		this.lifeTime = lifeTime;
+	}
+
+	public LifeLimitedParticle(final int x, final int y, final int lifeTime, final int layer,
+			final BufferedImage texture) {
+		super(x, y, layer, texture);
+		this.lifeTime = lifeTime;
+	}
+
+	@Override
+	public void move(double dx, double dy) {
+		if (isAlive()) { // Si la particule est en vie.
+			super.move(dx, dy);
+			if (lifeTime != -1) // Si la vie n'est pas immortelle.
+				life++; // Incrémentation du temps de vie à chaque déplacement.
+		}
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		if (isAlive()) // On ne dessine la particule que si elle est encore en vie.
+			super.draw(g);
+	}
+
+	public boolean isAlive() {
+		return life < lifeTime || lifeTime == -1; // Renvoie True si la particule est en vie ou immortelle.
+	}
+
+}
