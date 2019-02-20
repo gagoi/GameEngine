b25122f05909db7127c1f9e4f37e1653eb7bfb04
Gagoi <zangla.jeremy@gmail.com>
2019-02-20 09:31:09 +0100

    UTIIIIILS
    Ajout d'un point en 2D (déplacable).
    Ajout d'un vecteur en 2D (fixe).
    Ajout d'une interface pour le déplacement des objets.

---
+++ fr/game_engine/utils/IMovable.java
@@ -0,0 +1,6 @@
+package fr.game_engine.utils;
+
+public interface IMovable {
+
+	public void move(double dx, double dy);
+}
---
+++ fr/game_engine/utils/PolygonD.java
@@ -0,0 +1,5 @@
+package fr.game_engine.utils;
+
+public class PolygonD {
+
+}
