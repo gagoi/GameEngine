0e14bf28f3283d2532a442baddeab34ad8bdfb0f
Gagoi <zangla.jeremy@gmail.com>
2019-02-21 13:31:35 +0100

    Fin du générateur de particules ?

--- fr/game_engine/graphics/elements/ParticleGenerator.java
+++ fr/game_engine/graphics/elements/ParticleGenerator.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import fr.game_engine.graphics.IRenderable;
-import fr.game_engine.utils.PolygonD;
+import fr.game_engine.utils.RectD;
 import fr.game_engine.utils.VectD;
 
 public class ParticleGenerator implements IRenderable {
 
 	private static final Random rand = new Random();
-	private PolygonD startingBox; // Position du générateur
+	private RectD startingBox; // Position du générateur
 	private ArrayList<LifeLimitedParticle> particles;
 	private final int nbParticlesPerGen;
 	private final int nbUpdateBetweenGen;
@@ -20,28 +20,46 @@ public class ParticleGenerator implements IRenderable {
 
 	private int counter = 0;
 
-	public ParticleGenerator(final VectD speed, final PolygonD startingBox,
-			final int nbParticlesPerGen, final int nbUpdateBetweenGen, final LifeLimitedParticle source) {
+	public ParticleGenerator(final VectD speed, final RectD startingBox, final int nbParticlesPerGen,
+			final int nbUpdateBetweenGen, final LifeLimitedParticle source) {
 		this.speed = speed;
 		this.startingBox = startingBox;
 		this.nbParticlesPerGen = nbParticlesPerGen;
 		this.nbUpdateBetweenGen = nbUpdateBetweenGen;
+
+		if (source == null)
+			throw new NullPointerException();
+
 		this.source = source;
 		this.particles = new ArrayList<LifeLimitedParticle>(nbParticlesPerGen);
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		//if (counter == 0)
-			//for (int i = 0; i < nbParticlesPerGen; i++)
-				//particles.add(new LifeLimitedParticle(rand.nextInt()source))
-				
+		for (LifeLimitedParticle lifeLimitedParticle : particles) {
+			lifeLimitedParticle.draw(g);
+		}
+		update();
+	}
+
+	private void update() {
+		if (counter == 0)
+			for (int i = 0; i < nbParticlesPerGen; i++)
+				particles.add(
+						new LifeLimitedParticle((int) startingBox.getX() + rand.nextInt((int) startingBox.getWidth()),
+								(int) startingBox.getY() + rand.nextInt((int) startingBox.getHeight()), source));
+		counter = (counter + 1) % nbUpdateBetweenGen;
+
+		for (int i = 0; i < particles.size(); i++) {
+			particles.get(i).move(speed.getX(), speed.getY());
+			if (!particles.get(i).isAlive())
+				particles.remove(i);
+		}
 	}
 
 	@Override
 	public int getLayer() {
-		// TODO Auto-generated method stub
-		return 0;
+		return source.layer;
 	}
 
 }
